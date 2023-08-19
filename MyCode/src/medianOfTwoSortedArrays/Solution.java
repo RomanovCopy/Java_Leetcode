72370b643ae0a7b77a596fd2cbe7c52367c84851
RomanovCopy <RomanovCopy@gmail.com>
2023-08-19 22:06:36 +0300

    end

--- MyCode/src/medianOfTwoSortedArrays/Solution.java
+++ MyCode/src/medianOfTwoSortedArrays/Solution.java
@@ -4,4 +4,61 @@ public class Solution {
     public Solution(){
 
     }
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+
+        // Убедитесь, что nums1 всегда имеет меньшую длину
+        if (m > n) {
+            int[] temp = nums1;
+            nums1 = nums2;
+            nums2 = temp;
+            m = nums1.length;
+            n = nums2.length;
+        }
+
+        int iMin = 0;
+        int iMax = m;
+        int halfLen = (m + n + 1) / 2;
+
+        while (iMin <= iMax) {
+            int i = (iMin + iMax) / 2;
+            int j = halfLen - i;
+
+            if (i < iMax && nums2[j - 1] > nums1[i]) {
+                iMin = i + 1; // i слишком маленькое
+            } else if (i > iMin && nums1[i - 1] > nums2[j]) {
+                iMax = i - 1; // i слишком большое
+            } else {
+                // i идеально
+
+                int maxLeft;
+                if (i == 0) {
+                    maxLeft = nums2[j - 1];
+                } else if (j == 0) {
+                    maxLeft = nums1[i - 1];
+                } else {
+                    maxLeft = Math.max(nums1[i - 1], nums2[j - 1]);
+                }
+
+                if ((m + n) % 2 == 1) {
+                    return maxLeft;
+                }
+
+                int minRight;
+                if (i == m) {
+                    minRight = nums2[j];
+                } else if (j == n) {
+                    minRight = nums1[i];
+                } else {
+                    minRight = Math.min(nums1[i], nums2[j]);
+                }
+
+                return (maxLeft + minRight) / 2.0;
+            }
+        }
+
+        return 0.0; // Если массивы пустые
+    }
 }
