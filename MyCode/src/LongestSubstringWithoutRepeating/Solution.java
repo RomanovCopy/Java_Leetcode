34f3b840a9a48624f0ebe8539cd5daec4e9b3ad2
RomanovCopy <RomanovCopy@gmail.com>
2023-08-19 14:26:34 +0300

    fix

--- MyCode/src/LongestSubstringWithoutRepeating/Solution.java
+++ MyCode/src/LongestSubstringWithoutRepeating/Solution.java
@@ -1,13 +1,26 @@
 package LongestSubstringWithoutRepeating;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
+
+    public Solution(){
+        lengthOfLongestSubstring("abcdeffghijklmnopqrstuvwxyzz");
+    }
 
     public int lengthOfLongestSubstring(String s) {
         int length=0;
-        
+        List<String> substrings=new ArrayList<>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+
+        }
+
+        System.out.println("Найдены подстроки: ");
+        for(String str:substrings){
+            length=str.length()>length?str.length():length;
+        }
         return length;
     }
-
-
-
 }
--- MyCode/src/Main.java
+++ MyCode/src/Main.java
@@ -2,8 +2,10 @@
 //без повторения символов.
 
 
+import LongestSubstringWithoutRepeating.Solution;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new Solution();
     }
 }
