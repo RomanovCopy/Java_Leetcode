a30240d62010b08391c32327689ce9e4c7a569d4
RomanovCopy <RomanovCopy@gmail.com>
2023-08-21 21:37:11 +0300

    fix

--- MyCode/src/Main.java
+++ MyCode/src/Main.java
@@ -1,7 +1,12 @@
+//Вернуть самую длинную подстроку палиндром из строки s
+
+
+import longestPalindromicSubstring.Solution;
+
 public class Main {
     public static void main(String[] args) {
 
-
+        new Solution("rommor");
 
     }
 }
--- MyCode/src/longestPalindromicSubstring/Solution.java
+++ MyCode/src/longestPalindromicSubstring/Solution.java
@@ -2,10 +2,38 @@ package longestPalindromicSubstring;
 
 public class Solution {
 
-    public Solution(){
-
+    public Solution(String s){
+        longestPalindrome(s);
     }
 
-    
+    public String longestPalindrome(String s) {
+        String max="";
+        if(s.length()<3){
+            return max;
+        }
+        for(int l=0;l<s.length()-2;l++){
+            for(int p=s.length()-1;p>l/2;p--){
+                var s1=s.substring(l,p);
+                if(s1.length()>max.length()) {
+                    if (isPalindrom(s1) && s1.length() > max.length()) {
+                        max = s1;
+                    }
+                }
+            }
+        }
+        return max;
+    }
+
+    private boolean isPalindrom(String palindrom){
+        boolean res=false;
+        for(int l=0; l<(int)palindrom.length()/2;l++){
+            for(int p=palindrom.length()-1;p>(int)palindrom.length()/2;p--){
+                res = palindrom.charAt(l)==palindrom.charAt(p);
+                if(!res)
+                    return false;
+            }
+        }
+        return res;
+    }
 
 }
