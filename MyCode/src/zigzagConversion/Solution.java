59a82e18189a1a4e19b4f69c5ebd08e85a835b3c
RomanovCopy <RomanovCopy@gmail.com>
2023-08-23 22:08:58 +0300

    fix

--- MyCode/src/zigzagConversion/Solution.java
+++ MyCode/src/zigzagConversion/Solution.java
@@ -3,12 +3,24 @@ package zigzagConversion;
 
 public class Solution {
 
-    public Solution(){
+    String text="PAYPALISHIRING";
+    Integer lines = 3;
 
+    public Solution(){
+        System.out.println(convert(text, lines));
     }
 
     public String convert(String s, int numRows) {
+        var chars=s.toCharArray();
+        int length=chars.length;
+        int runner=0;
+        int anchor=1;
+        int step=numRows+numRows-2;
+        while (anchor<s.length()){
 
+        }
+
+        return res;
     }
 
 }
