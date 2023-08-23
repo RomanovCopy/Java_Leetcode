cc35cfcab000113cc46022c523d65b5a17535cd0
RomanovCopy <RomanovCopy@gmail.com>
2023-08-23 19:46:07 +0300

    start

--- MyCode/src/Main.java
+++ MyCode/src/Main.java
@@ -1,5 +1,21 @@
+import zigzagConversion.Solution;
+
+//Строка «PAYPALISHIRING» написана зигзагообразным узором на заданном числе строк,
+// например: (для лучшей разборчивости можно отобразить этот узор фиксированным шрифтом)
+
+//        P   A   H   N
+//        A P L S I I G
+//        Y   I   R
+
+//А затем читать этот текст построчно: «PAHNAPLSIIGYIR»
+//Запишите код, который будет принимать строку, и выполните это преобразование,
+// указав количество строк:
+
+//   string convert(string s, int numRows);
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new Solution();
     }
 }
---
+++ MyCode/src/zigzagConversion/Solution.java
@@ -0,0 +1,14 @@
+package zigzagConversion;
+
+
+public class Solution {
+
+    public Solution(){
+
+    }
+
+    public String convert(String s, int numRows) {
+
+    }
+
+}
