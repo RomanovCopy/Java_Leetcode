ebb9bb66d871c93b395d3e6dbdbd2d9a1422822c
RomanovCopy <RomanovCopy@gmail.com>
2023-09-04 19:39:03 +0300

    END

--- MyCode/src/zigzagConversion/Solution.java
+++ MyCode/src/zigzagConversion/Solution.java
@@ -11,16 +11,34 @@ public class Solution {
     }
 
     public String convert(String s, int numRows) {
-        var chars=s.toCharArray();
-        int length=chars.length;
-        int runner=0;
-        int anchor=1;
-        int step=numRows+numRows-2;
-        while (anchor<s.length()){
-
+        if (numRows == 1 || s.length() <= numRows) {
+            return s;
         }
 
-        return res;
+        StringBuilder[] rows = new StringBuilder[numRows];
+        for (int i = 0; i < numRows; i++) {
+            rows[i] = new StringBuilder();
+        }
+
+        int currentRow = 0;
+        boolean goingDown = false;
+
+        for (char c : s.toCharArray()) {
+            rows[currentRow].append(c);
+
+            if (currentRow == 0 || currentRow == numRows - 1) {
+                goingDown = !goingDown;
+            }
+
+            currentRow += goingDown ? 1 : -1;
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (StringBuilder row : rows) {
+            result.append(row);
+        }
+
+        return result.toString();
     }
 
 }
