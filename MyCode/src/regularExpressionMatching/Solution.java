304cc2fe9526d1513c3908d0772c621cfcea217e
RomanovCopy <RomanovCopy@gmail.com>
2023-10-01 18:52:34 +0300

    fix

--- MyCode/src/regularExpressionMatching/Solution.java
+++ MyCode/src/regularExpressionMatching/Solution.java
@@ -11,17 +11,41 @@ public class Solution {
 
     public Solution() {
 
-        String str="";
-        String reg="";
-        System.out.println(isMatch(str,reg));
+        String s = "aab";
+        String p = "c*a*b";
+        boolean result = isMatch(s, p);
+        System.out.println("Сопоставление: " + result);
 
     }
 
     public boolean isMatch(String s, String p) {
-        boolean result=false;
-        
-        return result;
+
+        int m = s.length();
+        int n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+
+        // Исходные значения для пустых строк
+        dp[0][0] = true;
+        for (int i = 1; i <= n; i++) {
+            if (p.charAt(i - 1) == '*') {
+                dp[0][i] = dp[0][i - 2];
+            }
+        }
+
+        // Заполнение таблицы DP
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                char sc = s.charAt(i - 1);
+                char pc = p.charAt(j - 1);
+
+                if (sc == pc || pc == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (pc == '*') {
+                    dp[i][j] = dp[i][j - 2] || (dp[i - 1][j] && (s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.'));
+                }
+            }
+        }
+
+        return dp[m][n];
     }
-
-
 }
