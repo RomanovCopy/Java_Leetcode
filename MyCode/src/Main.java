b2a368dae3be25b5e6bee058c3ce8206bbb738e6
RomanovCopy <RomanovCopy@gmail.com>
2023-10-01 18:09:48 +0300

    start

--- MyCode/src/Main.java
+++ MyCode/src/Main.java
@@ -1,4 +1,4 @@
-import stringToInteger_atoi.*;
+import regularExpressionMatching.Solution;
 
 public class Main {
     public static void main(String[] args) {
---
+++ MyCode/src/regularExpressionMatching/Solution.java
@@ -0,0 +1,27 @@
+package regularExpressionMatching;
+
+/* Учитывая входную строку s и шаблон p, реализуйте сопоставление регулярных выражений с
+ * поддержкой символов '.' и '*', где:
+ *
+ * '.' Соответствует любому одиночному символу.
+ * '*' Соответствует нулю или более предшествующим элементам.
+ * Сопоставление должно охватывать всю входную строку (не частичное сопоставление).*/
+
+public class Solution {
+
+    public Solution() {
+
+        String str="";
+        String reg="";
+        System.out.println(isMatch(str,reg));
+
+    }
+
+    public boolean isMatch(String s, String p) {
+        boolean result=false;
+        
+        return result;
+    }
+
+
+}
