c1507902d0ab7bcc0bf47aefecdfef32dbb50887
RomanovCopy <RomanovCopy@gmail.com>
2023-10-09 22:24:38 +0300

    fix

--- MyCode/src/maximumAmountOfWater/Solution.java
+++ MyCode/src/maximumAmountOfWater/Solution.java
@@ -1,6 +1,9 @@
 package maximumAmountOfWater;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 /**
  * Вам дан целочисленный массив height длиной n. Нарисованы n вертикальных линий так,
@@ -20,24 +23,40 @@ public class Solution {
 
     public Solution() {
 
-        height = new int[]{1, 2, 1};
+        height = createArray(10);
         System.out.println(maxArea(height));
     }
 
     public int maxArea(int[] height) {
         int max = 0;
-        int temp=0;
-        int length= height.length;
-        for(int i=0;i<length;i++){
-            for(int j=0;j<length;j++){
-                temp=height[i]<=height[j]?
-                        height[i]*Math.abs(i-j):
-                        height[j]*Math.abs(i-j);
-                max=temp>max?temp:max;
-            }
-        }
+        
         return max;
     }
+
+    public int[]createArray(int size){
+        List<Integer>list=new ArrayList<>();
+        Random random=new Random();
+        for(int i=0;i<size;i++){
+            list.add(random.nextInt(1,25));
+        }
+        return Arrays.stream(list.toArray(new Integer[list.size()])).mapToInt(Integer::intValue).toArray();
+    }
+
+
+//    public int maxArea(int[] height) {
+//        int max = 0;
+//        int temp=0;
+//        int length= height.length;
+//        for(int i=0;i<length;i++){
+//            for(int j=0;j<length;j++){
+//                temp=height[i]<=height[j]?
+//                        height[i]*Math.abs(i-j):
+//                        height[j]*Math.abs(i-j);
+//                max=temp>max?temp:max;
+//            }
+//        }
+//        return max;
+//    }
 
 
 }
